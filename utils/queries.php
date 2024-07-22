f906a464a6a135343906ecd741594ad4b22cbf93
mmccjjj <dhuber88@sunrise.ch>
2024-07-22 16:58:30 +0200

    fdfs

--- utils/queries.php
+++ utils/queries.php
@@ -3,6 +3,13 @@
     INCLUDE DATABASE CONNECTION
 */
 include('utils/db.php');
+include('utils/helper.php');
 
-$query= "SELECT "
+
+$query= "SELECT * FROM questionsAlt WHERE type = 'animals'";
+$stmt= $dbConn->prepare($query);
+$stmt->execute();
+$questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$questionsClean = array_column($questions, null, 'id');
 
